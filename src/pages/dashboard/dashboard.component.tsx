503511ae9eb569c92177ce0ec7c50772d2537f6b
Andela-Didacus <didacus.abongo@andela.com>
2017-10-09 17:01:30 +0300

    feat(genua): create about us section

--- src/pages/dashboard/dashboard.component.tsx
+++ src/pages/dashboard/dashboard.component.tsx
@@ -89,35 +89,83 @@ export class DashboardComponent extends React.Component<any, any> {
           <div className="about-us">
             <div className="mission">
               <h2>Our Mission</h2>
-              <p>To identify  new opportunities that will be of benefit to all genders               <br />
-                To engage  faculty, staff and students in international opportunities to
-                promote capacity  <br/> building and knowledge transfer, <br/> and to contribute to their learning and
-                understanding of global issues<br />
-                To manage  and develop proposals for submission to funding agencies<br />
-                To provide  and train on sound leadership expertise</p>
-              <p>To  ensure GENUA involvement in projects which are ethically sound, gender
-                <br/>inclusive and environmentally sustainable </p>
-              <p>To develop  opportunities to enhance teaching, research and community service</p>
+              <p>To identify  new opportunities that will be of benefit to all genders.</p>
+              <p>To engage  faculty, staff and students in international opportunities
+                to promote capacity building and knowledge transfer,
+                and to contribute to their learning and
+                understanding of global issues.</p>
+              <p>To manage  and develop proposals for submission to funding agencies.</p>
+              <p>To provide  and train on sound leadership expertise.</p>
+              <p>To  ensure GENUA involvement in projects which are ethically sound,
+                gender inclusive and environmentally sustainable. </p>
+              <p>To develop  opportunities to enhance teaching, research and community service.</p>
               <p><strong>The purpose of the network was as follows:</strong></p>
               <ul>
-                <li> To  form a network of women around Africa and beyond <br/>
-                  who will rely on each other to  advance their ambition</li>
-                <li> For  collaboration and Partnership</li>
-                <li> To  carry out research together in issues that is of relevance to the society</li>
-                <li> Hold  workshops and conferences</li>
-                <li> Start  journals for publishing academic work</li>
-                <li> Start  a gender magazine</li>
-                <li> To  form linkages and networks with other relevant
-                  organizations <br/> and bodies for  fundraising purposes in gender and higher education</li>
-                <li> To  engage in mentorship and career Guidance</li>
-                <li> Guidance counseling and counseling services</li>
+                <li>To form a network of women around Africa and beyond
+                  who will rely on each other to  advance their ambition.</li>
+                <li>For  collaboration and Partnership.</li>
+                <li>To  carry out research together in issues that is of relevance to the society.</li>
+                <li>Hold  workshops and conferences.</li>
+                <li>Start  journals for publishing academic work.</li>
+                <li>Start  a gender magazine.</li>
+                <li>To  form linkages and networks with other relevant
+                  organizations and bodies for  fundraising purposes in gender and higher education</li>
+                <li>To  engage in mentorship and career Guidance.</li>
+                <li>Guidance counseling and counseling services.</li>
               </ul>
             </div>
             <div className="image">
             </div>
           </div>
           <div className="team">
-            Team
+            <div className="person-tag">
+              <div className="person-image one"></div>
+              <div className="person-info">
+                <span><strong>Prof. Anne Asuquo</strong></span>
+                <br/>
+                <span>Chairperson</span>
+                <br/>
+                <span>University of Calabar</span>
+                <br/>
+                <span>Nigeria</span>
+              </div>
+            </div>
+            <div className="person-tag">
+              <div className="person-image two"></div>
+              <div className="person-info">
+                <span><strong>Dr. Anne Achieng Aseey</strong></span>
+                <br/>
+                <span>Organising Secretary</span>
+                <br/>
+                <span>University of Nairobi</span>
+                <br/>
+                <span>Kenya</span>
+              </div>
+            </div>
+            <div className="person-tag">
+              <div className="person-image one"></div>
+              <div className="person-info">
+                <span><strong>Prof. Mary Walingo</strong></span>
+                <br/>
+                <span>Alternate Chair</span>
+                <br/>
+                <span>Maasai Mara University</span>
+                <br/>
+                <span>Kenya</span>
+              </div>
+            </div>
+            <div className="person-tag">
+              <div className="person-image one"></div>
+              <div className="person-info">
+                <span><strong>Mrs. Georgina Mundia</strong></span>
+                <br/>
+                <span>Treasurer</span>
+                <br/>
+                <span>Copperstone University</span>
+                <br/>
+                <span>Zambia</span>
+              </div>
+            </div>
           </div>
           <div className="contacts">
             Contacts
